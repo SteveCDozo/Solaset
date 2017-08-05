cc5267fa0aefa6be48863aa39fc9f3aa77010b8a
Steve Cardozo <cardos@rpi.edu>
2017-08-05 16:20:19 -0400

    Manage Projects Table - individual box changes
    Gave boxes white backgrounds
    Gave them shadows

--- ghanaprojectexample.php
+++ ghanaprojectexample.php
@@ -1,5 +1,5 @@
 <div class="col-md-4">
-  <article>
+  <article style="background-color:white;padding: 10px 10px 10px 10px; margin-top: 10px; margin-bottom: 10px;box-shadow: 0px 0px 5px #bab8b8">
     <img class="img img-responsive" src="http://citifmonline.com/wp-content/uploads/2014/05/UG-111.jpg">
     <h4>University of Ghana, 10KW</h4>
     <p>UG wants to solar power their new science building so students can do labs without the light going off on them..</p>
