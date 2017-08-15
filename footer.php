e0b689e5753b52b713342b139333e7faa3370d79
SteveCDozo <cardos@rpi.edu>
2017-08-15 14:06:51 -0400

    Updated 'footer.php' to use new 'portal' var

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div class="footer navbar navbar-default">
   <div class="container">
-    <?php if(!$command): ?>
+    <?php if($portal != "admin"): ?>
       <p class="text-muted text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse et ex vehicula, ultrices quam id, sodales nibh. Aliquam ipsum diam, convallis ut neque quis, placerat tempor enim. Fusce ornare nunc vitae est aliquet, id cursus ante varius. Nulla facilisi. Nullam tempus, mauris in dapibus aliquet, enim velit dignissim odio, nec vulputate est sem mollis nunc. Sed finibus ligula ut aliquet ultricies. Nulla facilisi. Nunc nibh lectus, consectetur et sodales iaculis, mollis sed dui. Morbi consequat dui dapibus lacus consectetur, eget dictum est aliquam. Aliquam sit amet lorem orci. Nullam a turpis eget justo aliquam convallis. Maecenas tincidunt aliquam magna et sodales. Ut in nunc rutrum nisi aliquet fermentum in quis ipsum.</p>
       <p class="text-muted text-left">Duis tincidunt euismod mi at lobortis. Maecenas nec vulputate tellus, ac pharetra massa. Vestibulum id dictum risus, non cursus tortor. Curabitur at purus in nibh pharetra ultricies. Curabitur ac tincidunt mauris, id ultricies lectus. Donec fermentum, nunc ut facilisis placerat, dolor urna viverra nibh, ut laoreet ipsum velit ac lacus. Phasellus vitae lacinia quam, sed congue arcu.</p>
     <?php endif; ?>
